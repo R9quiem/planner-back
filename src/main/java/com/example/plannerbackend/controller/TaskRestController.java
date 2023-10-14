aada08b3e062b357c1e6b13c5a497a97cf34ba40
efim <efimburachevsky@gmail.com>
2023-10-15 01:10:11 +0300

    updated response of GET("/tasks/{id}"), if task not found, then return HttpStatus.NOT_FOUND(404)

--- src/main/java/com/example/plannerbackend/controller/TaskRestController.java
+++ src/main/java/com/example/plannerbackend/controller/TaskRestController.java
@@ -3,6 +3,7 @@ package com.example.plannerbackend.controller;
 import com.example.plannerbackend.entity.Task;
 import com.example.plannerbackend.service.TaskServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,7 +23,10 @@ public class TaskRestController {
 
     @GetMapping("/{id}")
     public ResponseEntity<Task> getTaskById(@PathVariable Long id) {
-        return ResponseEntity.ok(taskService.getTaskById(id));
+        Task task = taskService.getTaskById(id);
+        return task != null
+                ? new ResponseEntity<>(task, HttpStatus.OK)
+                : new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
     @PostMapping
