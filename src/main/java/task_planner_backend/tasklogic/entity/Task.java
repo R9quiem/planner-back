e685caea2986e51a352b811c29f6a53cb36e79b3
efim <efimburachevsky@gmail.com>
2023-10-18 22:10:57 +0300

    updated annotations in class Task for DB

--- src/main/java/task_planner_backend/tasklogic/entity/Task.java
+++ src/main/java/task_planner_backend/tasklogic/entity/Task.java
@@ -1,5 +1,6 @@
 package task_planner_backend.tasklogic.entity;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,10 +9,11 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Date;
 
 @Data
 @Entity
-@Table
+@Table(name = "tasks")
 @AllArgsConstructor
 @NoArgsConstructor
 public class Task {
@@ -20,13 +22,30 @@ public class Task {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-
+    @Column(name = "name")
     private String name;
 
-    private LocalDate date;
+    @Column(name = "description")
+    private String description;
 
+    @Column(name = "date")
+    @Temporal(TemporalType.DATE)
+    private Date date;
+
+    @Column(name = "dateCreatedAt")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+
+    @Column(name = "priority")
     private String priority;
 
+    @Column(name = "state")
     private String state;
+
+    @PrePersist
+    public void prePersist() {
+        // Устанавливаем поле date на текущую дату при создании задачи
+        date = new Date();
+    }
 
 }
