5caf044cc2b2562e73b2454c33871e5a1a3b5a9c
efim <efimburachevsky@gmail.com>
2023-10-19 17:33:33 +0300

    added type of task "abandoned"

--- src/main/java/task_planner_backend/auth/model/User.java
+++ src/main/java/task_planner_backend/auth/model/User.java
@@ -1,19 +1,21 @@
 package task_planner_backend.auth.model;
 
 import lombok.Data;
-import task_planner_backend.tasklogic.entity.Task;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.util.List;
 
+import task_planner_backend.tasklogic.entity.Task;
+
 @Table(name = "users",
         uniqueConstraints =  {
-                @UniqueConstraint(columnNames = "name"),
+                @UniqueConstraint(columnNames = "username"),
                 @UniqueConstraint(columnNames = "email")
-
 })
 @Entity
 @Data
+@NoArgsConstructor
 public class User {
 
     @Id
@@ -23,6 +25,11 @@ public class User {
     private String username;
     private String email;
     private String password;
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
 
     @OneToMany
     private List<Task> tasks;
